4dc8b405fc10bcc82ce88ec4351f4b715c640a47
Lucas Vocos <lucas.vocos@me.com>
2019-05-09 13:38:23 -0400

    create js file shells

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/* global bookmark, api, store */
+'use strict';
+$(() => {
+  bookmark.bindEventListeners();
+  bookmark.render();
+  api.getItems()
+    .then((items) =>{
+      items.forEach(item => store.addItem(item));
+      bookmark.render();
+    });
+});
